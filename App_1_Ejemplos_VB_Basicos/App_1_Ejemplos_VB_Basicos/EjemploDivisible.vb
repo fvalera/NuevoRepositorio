ac5d5f303e53ef1b5ebb847bda32526b054e8b5c
sinensia107 <sinensia107@VINET07>
2017-06-22 19:46:20 +0200

    Ejemplos Divisible y Signo

---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/EjemploDivisible.vb
@@ -0,0 +1,27 @@
+﻿Module EjemploDivisible
+    Sub Main()
+
+        Dim x As Integer
+        Dim contador As Integer
+
+        Console.WriteLine("Escriba el numero")
+        x = Console.ReadLine()
+
+        contador = 1
+
+        While contador <= 100
+
+            If (contador Mod x = 0) Then
+
+                Console.WriteLine(contador & " Es divisible entre " & x)
+
+            End If
+
+            contador = contador + 1
+        End While
+
+        Console.Read()
+
+    End Sub
+
+End Module
---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/EjemploSigno.vb
@@ -0,0 +1,41 @@
+﻿Module EjemploSigno
+    Sub Main()
+
+        Dim x As Integer
+        Dim y As Integer
+        Dim comprobador As Boolean
+
+        Console.WriteLine("Escriba el primer numero")
+        x = Console.ReadLine()
+
+        Console.WriteLine("Escriba el segundo numero")
+        y = Console.ReadLine()
+
+        Console.WriteLine("El valor maximo es: " & Math.Max(x, y))
+        Console.WriteLine("El valor minimo es: " & Math.Min(x, y))
+
+        Console.WriteLine("El signo del primer numero es: " & If(Math.Sign(x) >= 0, "Positivo", "Negativo"))
+        Console.WriteLine("El signo del segundo numero es: " & If(Math.Sign(y) >= 0, "Positivo", "Negativo"))
+
+        If Math.Sign(x) = Math.Sign(y) Then
+
+            comprobador = True
+
+        Else
+
+            comprobador = False
+
+        End If
+
+        Select Case comprobador
+            Case True
+                Console.WriteLine("Los signos de ambos números son iguales")
+            Case False
+                Console.WriteLine("Los signos de ambos números son distintos")
+        End Select
+
+        Console.Read()
+
+    End Sub
+
+End Module
