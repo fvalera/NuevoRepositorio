7e7c0ad66b16256a56e283a1ecd00b230870338e
sinensia107 <sinensia107@VINET07>
2017-06-22 19:41:03 +0200

    Ejemplos Booleanos, If, IfString e IfNulo

---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/EjemploIfNulo.vb
@@ -0,0 +1,17 @@
+﻿Module EjemploIfNulo
+    Sub main()
+        Dim ciudad As String
+        Dim etiqueta As String = If(ciudad, "indefinida")
+
+        'Muestra "indefinida" 
+        Console.WriteLine(etiqueta)
+
+        ciudad = "VALENCIA"
+        etiqueta = If(ciudad, "indefinida")
+
+        'Muestra "VALENCIA" 
+        Console.WriteLine(etiqueta)
+        System.Console.Read()
+    End Sub
+
+End Module
---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/EjemploIfString.vb
@@ -0,0 +1,19 @@
+﻿Module EjemploIfString
+    Sub Main()
+        Dim numClientes As Integer = 98
+        Dim etiqueta As Integer = If(numClientes <= 100,
+        "Menos de 100 clientes", "Al menos 100 clientes")
+        'Muestra "Menos de 100 clientes" 
+        Console.WriteLine(etiqueta)
+
+        numClientes = 120
+        etiqueta = If(numClientes <= 100, "Menos de 100 clientes",
+        "Al menos 100 clientes")
+
+        'Muestra "Al menos 100 clientes" 
+        Console.WriteLine(etiqueta)
+
+        System.Console.Read()
+    End Sub
+
+End Module
