8674d4f363b4675633e8c963d8d9c3c3a5d627d9
German Caballero <gcaballero@pronoide.es>
2017-06-28 20:07:12 +0200

    Ejercicios con Formularios y eventos 28/07/2017

---
+++ App_4_Ejemplo_WinForm_ListBox/App_4_Ejemplo_WinForm_ListBox/FormPrincipal.Designer.vb
@@ -0,0 +1,399 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtMultilinea = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDiezCaracteres = New System.Windows.Forms.TextBox()
+        Me.btnMostrarLongitud = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblLongitudes = New System.Windows.Forms.Label()
+        Me.lstListaDatos = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnAdd2 = New System.Windows.Forms.Button()
+        Me.btnEliminarDeLista = New System.Windows.Forms.Button()
+        Me.btnAbrirForm = New System.Windows.Forms.Button()
+        Me.btnCerrar = New System.Windows.Forms.Button()
+        Me.btnCambiar = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NuevoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AbrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BuscarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtMultilinea
+        '
+        Me.txtMultilinea.Location = New System.Drawing.Point(18, 59)
+        Me.txtMultilinea.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtMultilinea.Name = "txtMultilinea"
+        Me.txtMultilinea.Size = New System.Drawing.Size(314, 26)
+        Me.txtMultilinea.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 34)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "TextoMultilinea"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 182)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(314, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Texto con longiud maxima de 10 caracteres"
+        '
+        'txtDiezCaracteres
+        '
+        Me.txtDiezCaracteres.Location = New System.Drawing.Point(18, 207)
+        Me.txtDiezCaracteres.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtDiezCaracteres.MaxLength = 10
+        Me.txtDiezCaracteres.Name = "txtDiezCaracteres"
+        Me.txtDiezCaracteres.Size = New System.Drawing.Size(314, 26)
+        Me.txtDiezCaracteres.TabIndex = 3
+        '
+        'btnMostrarLongitud
+        '
+        Me.btnMostrarLongitud.Location = New System.Drawing.Point(18, 243)
+        Me.btnMostrarLongitud.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnMostrarLongitud.Name = "btnMostrarLongitud"
+        Me.btnMostrarLongitud.Size = New System.Drawing.Size(176, 35)
+        Me.btnMostrarLongitud.TabIndex = 4
+        Me.btnMostrarLongitud.Text = "Mostrar longitud"
+        Me.btnMostrarLongitud.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 289)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Longitudes:"
+        '
+        'lblLongitudes
+        '
+        Me.lblLongitudes.AutoSize = True
+        Me.lblLongitudes.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLongitudes.Location = New System.Drawing.Point(22, 319)
+        Me.lblLongitudes.Name = "lblLongitudes"
+        Me.lblLongitudes.Size = New System.Drawing.Size(90, 18)
+        Me.lblLongitudes.TabIndex = 6
+        Me.lblLongitudes.Text = "Longitudes"
+        '
+        'lstListaDatos
+        '
+        Me.lstListaDatos.FormattingEnabled = True
+        Me.lstListaDatos.ItemHeight = 20
+        Me.lstListaDatos.Items.AddRange(New Object() {"Perro", "Gato", "Murciégalo", "Ratón", "Hámster"})
+        Me.lstListaDatos.Location = New System.Drawing.Point(532, 79)
+        Me.lstListaDatos.Name = "lstListaDatos"
+        Me.lstListaDatos.Size = New System.Drawing.Size(130, 104)
+        Me.lstListaDatos.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(416, 35)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(113, 20)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Lista de datos:"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(339, 59)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(71, 31)
+        Me.btnAdd.TabIndex = 9
+        Me.btnAdd.Text = "Añadir"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnAdd2
+        '
+        Me.btnAdd2.Location = New System.Drawing.Point(339, 202)
+        Me.btnAdd2.Name = "btnAdd2"
+        Me.btnAdd2.Size = New System.Drawing.Size(71, 31)
+        Me.btnAdd2.TabIndex = 10
+        Me.btnAdd2.Text = "Añadir"
+        Me.btnAdd2.UseVisualStyleBackColor = True
+        '
+        'btnEliminarDeLista
+        '
+        Me.btnEliminarDeLista.Enabled = False
+        Me.btnEliminarDeLista.Location = New System.Drawing.Point(587, 236)
+        Me.btnEliminarDeLista.Name = "btnEliminarDeLista"
+        Me.btnEliminarDeLista.Size = New System.Drawing.Size(75, 42)
+        Me.btnEliminarDeLista.TabIndex = 11
+        Me.btnEliminarDeLista.Text = "Eliminar"
+        Me.btnEliminarDeLista.UseVisualStyleBackColor = True
+        '
+        'btnAbrirForm
+        '
+        Me.btnAbrirForm.Location = New System.Drawing.Point(339, 289)
+        Me.btnAbrirForm.Name = "btnAbrirForm"
+        Me.btnAbrirForm.Size = New System.Drawing.Size(194, 37)
+        Me.btnAbrirForm.TabIndex = 12
+        Me.btnAbrirForm.Text = "Abrir Form Combo Listas"
+        Me.btnAbrirForm.UseVisualStyleBackColor = True
+        '
+        'btnCerrar
+        '
+        Me.btnCerrar.Location = New System.Drawing.Point(621, 319)
+        Me.btnCerrar.Name = "btnCerrar"
+        Me.btnCerrar.Size = New System.Drawing.Size(41, 37)
+        Me.btnCerrar.TabIndex = 13
+        Me.btnCerrar.Text = "X"
+        Me.btnCerrar.UseVisualStyleBackColor = True
+        '
+        'btnCambiar
+        '
+        Me.btnCambiar.Location = New System.Drawing.Point(214, 243)
+        Me.btnCambiar.Name = "btnCambiar"
+        Me.btnCambiar.Size = New System.Drawing.Size(118, 35)
+        Me.btnCambiar.TabIndex = 14
+        Me.btnCambiar.Text = "Cambiar"
+        Me.btnCambiar.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(344, 118)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(307, 20)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Texto largo que no entra en label pequeño"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(242, 364)
+        Me.DateTimePicker1.MaxDate = New Date(2018, 12, 31, 0, 0, 0, 0)
+        Me.DateTimePicker1.MinDate = New Date(2016, 1, 1, 0, 0, 0, 0)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(345, 26)
+        Me.DateTimePicker1.TabIndex = 16
+        '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(18, 364)
+        Me.MonthCalendar1.MaxSelectionCount = 100
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 17
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem, Me.EditarToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(864, 24)
+        Me.MenuStrip1.TabIndex = 18
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoToolStripMenuItem, Me.AbrirToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.ArchivoToolStripMenuItem.Text = "Archivo"
+        '
+        'NuevoToolStripMenuItem
+        '
+        Me.NuevoToolStripMenuItem.Name = "NuevoToolStripMenuItem"
+        Me.NuevoToolStripMenuItem.Size = New System.Drawing.Size(109, 22)
+        Me.NuevoToolStripMenuItem.Text = "Nuevo"
+        '
+        'AbrirToolStripMenuItem
+        '
+        Me.AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        Me.AbrirToolStripMenuItem.Size = New System.Drawing.Size(109, 22)
+        Me.AbrirToolStripMenuItem.Text = "Abrir"
+        '
+        'EditarToolStripMenuItem
+        '
+        Me.EditarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BuscarToolStripMenuItem})
+        Me.EditarToolStripMenuItem.Name = "EditarToolStripMenuItem"
+        Me.EditarToolStripMenuItem.Size = New System.Drawing.Size(49, 20)
+        Me.EditarToolStripMenuItem.Text = "Editar"
+        '
+        'BuscarToolStripMenuItem
+        '
+        Me.BuscarToolStripMenuItem.Name = "BuscarToolStripMenuItem"
+        Me.BuscarToolStripMenuItem.Size = New System.Drawing.Size(109, 22)
+        Me.BuscarToolStripMenuItem.Text = "Buscar"
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.Location = New System.Drawing.Point(242, 500)
+        Me.DateTimePicker2.MaxDate = New Date(2018, 12, 31, 0, 0, 0, 0)
+        Me.DateTimePicker2.MinDate = New Date(2016, 1, 1, 0, 0, 0, 0)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(345, 26)
+        Me.DateTimePicker2.TabIndex = 19
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(242, 417)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(57, 20)
+        Me.Label6.TabIndex = 20
+        Me.Label6.Text = "Label6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(242, 452)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(57, 20)
+        Me.Label7.TabIndex = 21
+        Me.Label7.Text = "Label7"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(636, 366)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(150, 26)
+        Me.TextBox1.TabIndex = 22
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(636, 398)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(150, 26)
+        Me.TextBox2.TabIndex = 23
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(636, 440)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 32)
+        Me.Button1.TabIndex = 24
+        Me.Button1.Text = "Comparar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(632, 488)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(57, 20)
+        Me.Label8.TabIndex = 25
+        Me.Label8.Text = "Label8"
+        '
+        'FormPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(864, 544)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.MonthCalendar1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnCambiar)
+        Me.Controls.Add(Me.btnCerrar)
+        Me.Controls.Add(Me.btnAbrirForm)
+        Me.Controls.Add(Me.btnEliminarDeLista)
+        Me.Controls.Add(Me.btnAdd2)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lstListaDatos)
+        Me.Controls.Add(Me.lblLongitudes)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnMostrarLongitud)
+        Me.Controls.Add(Me.txtDiezCaracteres)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtMultilinea)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "FormPrincipal"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtMultilinea As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtDiezCaracteres As TextBox
+    Friend WithEvents btnMostrarLongitud As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblLongitudes As Label
+    Friend WithEvents lstListaDatos As ListBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnAdd2 As Button
+    Friend WithEvents btnEliminarDeLista As Button
+    Friend WithEvents btnAbrirForm As Button
+    Friend WithEvents btnCerrar As Button
+    Friend WithEvents btnCambiar As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents MonthCalendar1 As MonthCalendar
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NuevoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AbrirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BuscarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label8 As Label
+End Class
---
+++ App_4_Ejemplo_WinForm_ListBox/App_4_Ejemplo_WinForm_ListBox/FormPrincipal.vb
@@ -0,0 +1,159 @@
+﻿Public Class FormPrincipal
+    Private miFormComboListas As FormComboListas
+    Private Sub FormPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        miFormComboListas = New FormComboListas()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnMostrarLongitud.Click
+        MessageBox.Show("longitud de texto multilena: " & txtMultilinea.TextLength.ToString() &
+                        vbCrLf & "longitud texto 2: " & txtDiezCaracteres.TextLength.ToString())
+    End Sub
+    Private Sub TextBox1_2_TextChanged(sender As Object, e As EventArgs) Handles txtMultilinea.TextChanged, txtDiezCaracteres.TextChanged
+        lblLongitudes.Text = "longitud de texto multilena: " & txtMultilinea.TextLength.ToString() &
+                        vbCrLf & "longitud texto 2: " & txtDiezCaracteres.TextLength.ToString()
+    End Sub
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        lstListaDatos.Items.Add(txtMultilinea.Text)
+    End Sub
+    Private Sub btnAdd2_Click(sender As Object, e As EventArgs) Handles btnAdd2.Click
+        lstListaDatos.Items.Add(txtDiezCaracteres.Text)
+        txtDiezCaracteres.Clear()
+    End Sub
+    Private Sub btnEliminarDeLista_Click(sender As Object, e As EventArgs) Handles btnEliminarDeLista.Click
+        lstListaDatos.Items.RemoveAt(lstListaDatos.SelectedIndex)
+    End Sub
+    Private Sub lstListaDatos_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstListaDatos.SelectedIndexChanged
+
+        btnEliminarDeLista.Enabled = (lstListaDatos.SelectedIndex >= 0)
+    End Sub
+    Private Sub btnAbrirForm_Click(sender As Object, e As EventArgs) Handles btnAbrirForm.Click
+        If miFormComboListas Is Nothing Or miFormComboListas.IsDisposed Then
+            miFormComboListas = New FormComboListas()
+        End If
+        miFormComboListas.ShowDialog()
+    End Sub
+    Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
+        miFormComboListas.Hide()  'nuevoFormComboListas.Close()
+    End Sub
+
+    Private Sub btnCambiar_Click(sender As Object, e As EventArgs) Handles btnCambiar.Click, btnMostrarLongitud.Click
+        Dim tipoFuente As Integer
+        'tipoFuente = System.Drawing.FontStyle.Bold + System.Drawing.FontStyle.Strikeout     ' 1001 = 9
+        tipoFuente = System.Drawing.FontStyle.Italic + System.Drawing.FontStyle.Underline   ' 0110 = 6
+        'tipoFuente = System.Drawing.FontStyle.Bold + System.Drawing.FontStyle.Italic        ' 0011 = 3
+
+        Me.lblLongitudes.Font = New Font("Microsoft Sans Serif", 16, tipoFuente,
+                                         System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        ' Ampliar el tamaño de txtMultilinea
+        Me.txtMultilinea.Multiline = True
+        Dim tamañoTxtMult As Size = Me.txtMultilinea.Size
+        tamañoTxtMult.Height += 100
+        Me.txtMultilinea.Size = tamañoTxtMult
+        ' Mover a la derecha el boton pulsado
+        Dim esteBoton As Button = CType(sender, Button)
+        esteBoton.Location = New Point(esteBoton.Location.X + 20, esteBoton.Location.Y)
+
+        ' Poner btnAbrirForm a la izquierda del boton Cerrar
+        btnAbrirForm.Location = New Point(btnCerrar.Location.X - btnAbrirForm.Size.Width - 10,
+                                            btnCerrar.Location.Y)
+
+        ' Cambiar las propiedades del propio formulario
+        Me.Text = "Nuevo titulo ventana"
+        Me.BackColor = Color.BurlyWood
+        Me.BackColor = Color.FromArgb(255, 255, 255, 0) '32 bits. 8 bit por color, 1 byte por color
+
+        '417; 45 / 245; 184
+        lstListaDatos.SetBounds(417, 45, 245, 185)
+        'lstListaDatos.BringToFront()
+        Label5.SendToBack()
+
+        '       Dim nuevoBoton As Button
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+        Dim fecha As Date = DateTimePicker1.Value
+        fecha = fecha.AddDays(1)
+
+        Me.Text = "Ventana: " & fecha.ToString()
+
+        If fecha.Year = 2017 Then
+            Me.Text += "¡Este mismo año!"
+        End If
+    End Sub
+
+    Private Sub NuevoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NuevoToolStripMenuItem.Click
+        Dim nuevoBoton As Button
+        nuevoBoton = New Button()
+        nuevoBoton.Size = New Size(200, 200)
+        nuevoBoton.Location = New Point(100, 100)
+        nuevoBoton.Text = "Pulsa para eliminar"
+        nuevoBoton.Parent = Me
+        nuevoBoton.BringToFront()
+        AddHandler nuevoBoton.Click, AddressOf EliminarComponente
+    End Sub
+    Private Sub EliminarComponente(sender As Button, e As EventArgs)
+        sender.Dispose()
+    End Sub
+
+    Private Sub MonthCalendar1_DateChanged(sender As Object, e As DateRangeEventArgs) Handles MonthCalendar1.DateChanged
+
+        DateTimePicker1.Value = MonthCalendar1.SelectionRange.Start
+        DateTimePicker2.Value = MonthCalendar1.SelectionRange.End
+
+    End Sub
+
+    Public Sub MostrarDiaSemana(dateTime As DateTimePicker, e As EventArgs) Handles DateTimePicker1.ValueChanged
+        Select Case dateTime.Value.DayOfWeek
+            Case DayOfWeek.Monday
+                Label6.Text = "Lunes"
+            Case DayOfWeek.Tuesday
+                Label6.Text = "Martes"
+            Case DayOfWeek.Wednesday
+                Label6.Text = "Miércoles"
+            Case DayOfWeek.Thursday
+                Label6.Text = "Jueves"
+            Case DayOfWeek.Friday
+                Label6.Text = "Viernes"
+            Case DayOfWeek.Saturday
+                Label6.Text = "Sábado"
+            Case DayOfWeek.Sunday
+                Label6.Text = "Domingo"
+        End Select
+    End Sub
+
+    Public Sub MostrarComparativa(dateTime As DateTimePicker, e As EventArgs) Handles DateTimePicker1.ValueChanged, DateTimePicker2.ValueChanged
+
+        If DateTimePicker1.Value.CompareTo(DateTimePicker2.Value) < 0 Then
+            Label7.Text = "La primera fecha es menor"
+        ElseIf DateTimePicker1.Value.CompareTo(DateTimePicker2.Value) > 0 Then
+            Label7.Text = "La primera fecha es mayor"
+        Else
+            Label7.Text = "Las fechas son iguales"
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If TextBox1.Text.CompareTo(TextBox2.Text) < 0 Then
+            Label8.Text = "Texto A está antes por orden alfabético"
+        ElseIf TextBox1.Text.CompareTo(TextBox2.Text) > 0 Then
+            Label8.Text = "Texto A está después por orden alfabético"
+        Else
+            Label8.Text = "Los textos son iguales"
+        End If
+
+    End Sub
+
+    Public Sub AlPulsarRaton(sender As Object, e As MouseEventArgs) Handles Me.MouseDown, Me.MouseMove
+
+        Dim nuevoBoton As New Button
+        nuevoBoton = New Button
+        nuevoBoton.Size = New Size(20, 20)
+        nuevoBoton.Text = "X"
+
+        nuevoBoton.Parent = Me
+        nuevoBoton.Location = e.Location
+
+
+    End Sub
+End Class
